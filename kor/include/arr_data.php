6222d45d518c302628f8edaab7e7eeb27db52ac5
furaha707 <zxcvyein950823@gmail.com>
2023-05-30 13:47:24 +0900

    Initial commit

---
+++ kor/include/arr_data.php
@@ -0,0 +1,126 @@
+<?
+
+$site_title = "(주)나이스페이먼츠"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = "(주)나이스페이먼츠"; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "070.123.4567";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "회사소개";
+$nav_1_1 = "대표이사 인사말";
+$nav_1_2 = "회사개요";
+$nav_1_3 = "NICE 가치체계";
+$nav_1_4 = "연혁";
+$nav_1_5 = "인재채용";
+$nav_1_5_1 = "인재채용";
+$nav_1_5_2 = "채용절차";
+$nav_1_6 = "오시는 길";
+
+$nav_2 = "사업영역";
+$nav_2_1 = "전자지급결제대행(PG) 사업";
+$nav_2_2 = "전자금융관련 사업";
+$nav_2_2_1 = "펌뱅킹";
+$nav_2_2_2 = "계좌간편결제";
+$nav_2_3 = "VAN 사업";
+$nav_2_4 = "CP 사업";
+$nav_2_4_1 = "Cross Border 결제";
+$nav_2_4_2 = "결제수단";
+
+$nav_3 = "서비스";
+$nav_3_1 = "PG 서비스";
+$nav_3_2 = "VAN 서비스";
+
+$nav_4 = "제품";
+$nav_4_1 = "유선단말기";
+$nav_4_2 = "무선단말기";
+$nav_4_3 = "POS";
+$nav_4_4 = "KIOSK";
+$nav_4_5 = "멀티패드";
+$nav_4_6 = "서명패드";
+$nav_4_7 = "IC 리더기";
+
+$nav_5 = "고객지원";
+$nav_5_1 = "공지사항";
+$nav_5_2 = "정도경영제보";
+$nav_5_3 = "사업제휴";
+
+$nav_6 = "입찰정보";
+$nav_6_1 = "입찰정보";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../about/ceo.html";
+$nav_1_1_href = "../about/ceo.html";
+$nav_1_2_href = "../about/introduce.html";
+$nav_1_3_href = "../about/mission.html";
+$nav_1_4_href = "../about/history.html";
+$nav_1_5_href = "../about/recruitment.html";
+$nav_1_5_1_href = "../about/recruitment.html";
+$nav_1_5_2_href = "../about/recruitment-process.html";
+$nav_1_6_href = "../about/location.html";
+
+$nav_2_href = "../business/business_1.html";
+$nav_2_1_href = "../business/business_1.html";
+$nav_2_2_href = "../business/business_2.html";
+$nav_2_2_1_href = "../business/business_2.html";
+$nav_2_2_2_href = "../business/business_2_1.html";
+$nav_2_3_href = "../business/business_3.html";
+$nav_2_4_href = "../business/business_4.html";
+$nav_2_4_1_href = "../business/business_4.html";
+$nav_2_4_2_href = "../business/business_4_1.html";
+
+$nav_3_href = "../service/pg.html";
+$nav_3_1_href = "../service/pg.html";
+$nav_3_2_href = "../service/van.html";
+
+$nav_4_href = "../product/list.html";
+$nav_4_1_href = "../product/list.html";
+$nav_4_2_href = "../product/list.html";
+$nav_4_3_href = "../product/list.html";
+$nav_4_4_href = "../product/list.html";
+$nav_4_5_href = "../product/list.html";
+$nav_4_6_href = "../product/list.html";
+$nav_4_7_href = "../product/list.html";
+
+$nav_5_href = "../support/notice.html";
+$nav_5_1_href = "../support/notice.html";
+$nav_5_2_href = "https://www.nice.co.kr/ko/sustainability/jebodetail";
+$nav_5_3_href = "../support/contact.html";
+
+$nav_6_href = "../info/list.html";
+$nav_6_1_href = "../info/list.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
